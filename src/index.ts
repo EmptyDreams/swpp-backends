0c023c0b0151dfe865c2924c62ef4ccd1f99dfb2
kmar <minedreams@qq.com>
2023-08-10 16:54:44 +0800

    更新插件版本号到 1.2.0

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ import {refreshUrl, analyze} from './VersionAnalyzer'
 
 // noinspection JSUnusedGlobalSymbols
 export default {
-    version: '1.1.2',
+    version: '1.2.0',
     cache: {
         readEjectData, readUpdateJson,
         readRules, readMergeVersionMap,
