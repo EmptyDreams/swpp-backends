c2a8e7fca1323279d0649b25782f25da80d121b4
kmar <minedreams@qq.com>
2023-12-02 23:15:25 +0800

    更新版本号到 `2.3.0-beta.1`

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ import {buildDomJs} from './DomBuilder'
 
 // noinspection JSUnusedGlobalSymbols
 export default {
-    version: '2.3.0-beta.0',
+    version: '2.3.0-beta.1',
     cache: {
         readEjectData, readUpdateJson,
         readRules, readMergeVersionMap,
