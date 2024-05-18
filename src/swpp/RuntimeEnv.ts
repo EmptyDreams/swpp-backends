e21d9045e181a24c2e781f972905b0b890056017
kmar <minedreams@qq.com>
2024-05-18 20:18:58 +0800

    以变量形式导出环境变量

--- src/swpp/RuntimeEnv.ts
+++ src/swpp/RuntimeEnv.ts
@@ -1,42 +1,46 @@
 import {utils} from './untils'
 
-/**
- * 读取环境变量的值
- * @throws RuntimeEnvException
- */
-export function readRuntimeEnv(key: string) {
-    const item = runtimeEnv[key]
-    if (!item) throw {key, message: 'key 不存在'}
-    const value = item.getter?.() ?? item.default
-    if (typeof value != typeof item.default)
-        throw {key, value, message: '用户传入的值类型与缺省值类型不统一'} as RuntimeEnvException<any>
-    const checkResult = item.checker?.(value)
-    if (checkResult) throw {key, ...checkResult}
-    return value
-}
+export const runtimeEnv = {
 
-/**
- * 设置环境变量的值
- * @throws RuntimeEnvException
- */
-export function writeRuntimeEnv(key: string, valueGetter: () => any) {
-    const item = runtimeEnv[key]
-    if (!item) throw {key, value: null, message: 'key 不存在'} as RuntimeEnvException<any>
-    item.getter = valueGetter
-}
+    /**
+     * 读取环境变量的值
+     * @throws RuntimeEnvException
+     */
+    read(key: string) {
+        const item = runtimeEnvMap[key]
+        if (!item) throw {key, message: 'key 不存在'}
+        const value = item.getter?.() ?? item.default
+        if (typeof value != typeof item.default)
+            throw {key, value, message: '用户传入的值类型与缺省值类型不统一'} as RuntimeEnvException<any>
+        const checkResult = item.checker?.(value)
+        if (checkResult) throw {key, ...checkResult}
+        return value
+    },
 
-/**
- * 追加环境变量
- * @throws RuntimeEnvException
- */
-export function appendRuntimeEnv<T>(key: string, env: RuntimeEnvValue<T>) {
-    if (key in runtimeEnv)
-        throw {key, value: runtimeEnv[key], message: 'key 重复'}
-    runtimeEnv[key] = env
+    /**
+     * 设置环境变量的值
+     * @throws RuntimeEnvException
+     */
+    update(key: string, valueGetter: () => any) {
+        const item = runtimeEnvMap[key]
+        if (!item) throw {key, value: null, message: 'key 不存在'} as RuntimeEnvException<any>
+        item.getter = valueGetter
+    },
+
+    /**
+     * 追加环境变量
+     * @throws RuntimeEnvException
+     */
+    append<T>(key: string, env: RuntimeEnvValue<T>) {
+        if (key in runtimeEnvMap)
+            throw {key, value: runtimeEnvMap[key], message: 'key 重复'}
+        runtimeEnvMap[key] = env
+    }
+
 }
 
 /** 环境变量 */
-const runtimeEnv: { [p: string]: RuntimeEnvValue<any> } = {
+const runtimeEnvMap: { [p: string]: RuntimeEnvValue<any> } = {
     CACHE_NAME: buildEnv({
         default: 'kmarBlogCache'
     }),
