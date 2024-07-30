618eb8c1604ba4cc53e33dd843cba79f5591b030
therehello <therehello@qq.com>
2024-07-30 14:21:07 +0800

    添加 `createUpdateChangeExps` 函数

--- src/swpp/JsonBuilder.ts
+++ src/swpp/JsonBuilder.ts
@@ -32,17 +32,74 @@ export class JsonBuilder {
 
 }
 
-function xxx(urls: Set<string>, refresh: Set<string>): UpdateChangeExp[] {
-    return [
-        {
-            flag: 'pre',
-            value: 'https://asdf'
-        },
-        {
-            flag: 'suf',
-            value: 'a.js'
+export function createUpdateChangeExps(urls: Set<string>, refresh: Set<string>): UpdateChangeExp[] {
+    interface Node {
+        next: (Node | undefined)[],
+        flag: boolean,
+        isEnd: boolean
+    }
+
+    function newNode(): Node {
+        return {
+            next: new Array(128),
+            flag: false,
+            isEnd: false
         }
-    ]
+    }
+
+    const head = newNode()
+    const insert = (content: string, flag: boolean) => {
+        let cur = head
+        for (let i = content.length - 1; i >= 0; i--) {
+            const index = content.charCodeAt(i)
+            if (cur.next[index]) {
+                cur = cur.next[index]!
+            } else {
+                cur = cur.next[index] = newNode()
+            }
+        }
+        cur.flag = flag
+        cur.isEnd = true
+    }
+    urls.forEach(it => insert(it, false))
+    refresh.forEach(it => insert(it, true))
+
+    function dfs(node: Node) {
+        if (node.isEnd) return
+        node.flag = true
+        for (let next of node.next) {
+            if (next) {
+                dfs(next)
+                node.flag = node.flag && next.flag
+            }
+        }
+    }
+
+    dfs(head)
+
+    let dfs2S: string[] = []
+    const result: UpdateChangeExp[] = []
+
+    function dfs2(node: Node) {
+        if (node.flag) {
+            result.push({
+                flag: 'suf',
+                value: dfs2S.reduceRight((a, b) => a + b, '')
+            })
+            return
+        }
+        for (let i = 0; i < node.next.length; i++) {
+            const next = node.next[i]
+            if (next) {
+                dfs2S.push(String.fromCharCode(i))
+                dfs2(next)
+                dfs2S.pop()
+            }
+        }
+    }
+
+    dfs2(head)
+    return result
 }
 
 export interface TrackerHeaderDiff {
