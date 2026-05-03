2de040108cfaaa2eeefe09c1972a49477bd1a82a
kmar <minedreams@qq.com>
2026-05-03 14:14:09 +0800

    fix: 修复 `gen_dom` 不填写时部分代码无法正确读取默认值的问题

--- src/swpp/cli.ts
+++ src/swpp/cli.ts
@@ -122,7 +122,7 @@ async function runBuild(cliJsonPath: string = './swpp.cli.json', context: 'dev' 
         context,
         publicPath: cliConfig.webRoot,
         isServiceWorker: cliConfig.serviceWorker ?? true,
-        domJsPath: cliConfig.gen_dom ? cliConfig.domJsPath : undefined,
+        domJsPath: cliConfig.gen_dom ? (cliConfig.domJsPath ?? '/sw-dom.js') : undefined,
         diffJsonPath: cliConfig.diffJsonPath
     })
     await actions.loadConfigs(Array.isArray(cliConfig.configFiles) ? cliConfig.configFiles : [cliConfig.configFiles])
